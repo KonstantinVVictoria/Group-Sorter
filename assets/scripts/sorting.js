8fad3e3a5abbf334ca5c1083caf8f196b650d1d1
KonstantinVVictoria <konstantinvictoria@gmail.com>
2022-02-08 02:06:24 -0800

    first commit

---
+++ assets/scripts/sorting.js
@@ -0,0 +1,281 @@
+const data = `Timestamp,Please provide your name,Please provide your personal email,Please provide your phone number,"On a scale from 1 – 9, how would you rank your experience with computer science?",Please choose your 1st preference,Please choose your 2nd preference,Please choose your 3rd preference
+1/22/2022 19:00:15,,tkpp26@gmail.com,6264897535,1,(General) App Development,Artificial Intelligence,Web Development
+1/27/2022 15:36:58,Riley Hui,rxhui992003@gmail.com,9257869986,1,(General) App Development,Game Development,Data Science
+1/31/2022 17:03:20,Justin Cox,jcox987@insite.4cd.edu,9258196042,1,"Anything, I am trying to get as much exposure as I can to figure it all out",Data Science,Artificial Intelligence
+1/31/2022 17:59:20,Nandini Velinedi ,nandinivelinedi03@gmail.com,5104354820,1,(General) App Development,Mobile App Development,Web Development
+2/1/2022 21:57:02,Jose S Soemarno,soemarnojose@gmail.com,9254905325,1,(General) App Development,Mobile App Development,Web Development
+1/22/2022 16:05:01,Jiwoo Hyun,lobotomy1019@gmail.com,9254460206,2,Mobile App Development,Web Development,Game Development
+1/22/2022 16:52:00,Kushi Kumbogowdana,kushikumbagowdana@gmail.com,9256219019,2,Artificial Intelligence,Web Development,Data Science
+1/22/2022 16:52:00,Pravilhika ,pravalhikak@gmail.com,9253070111,2,Artificial Intelligence,Web Development,Data Science
+1/22/2022 18:57:04,Angie Le,wildhorse8052@gmail.com,9253053182,2,Game Development,(General) App Development,VR
+1/31/2022 17:33:04,Anne Hyunsun Joo,spdlqjdyd0414@naver.com,9258222596,2,Web Development,Artificial Intelligence,Game Development
+2/4/2022 16:46:14,Ardan Giordjie,lifeofardan@gmail.com,9252148655,2,(General) App Development,Artificial Intelligence,Web Development
+2/7/2022 17:43:22,Coline Depagne,coline.depagne@gmail.com,9254287016,2,Data Science,Artificial Intelligence,Game Development
+1/23/2022 16:12:51,Jacob Willemsz,jacobwillemsz101@gmail.com,9254288690,3,(General) App Development,Mobile App Development,Artificial Intelligence
+1/27/2022 17:11:21,Joseph Mamondol,itsjmamondol@gmail.com,9254087040,3,Web Development,Data Science,(General) App Development
+1/28/2022 21:16:18,Anna Doan,anna@davedoan.com,5109289998,3,my experience is super limited to what ik in a classroom :( i’m down to learn though!,my experience is super limited to what ik in a classroom :( i’m down to learn though!,my experience is super limited to what ik in a classroom :( i’m down to learn though!
+1/29/2022 19:05:04,Thomas Yeoh,zyeoh029@insite.4cd.edu,6697997334,3,(General) App Development,Artificial Intelligence,Web Development
+2/4/2022 13:35:37,Thomas Lin,tlin879@insite.4cd.net,9259894416,3,(General) App Development,Web Development,Game Development
+2/6/2022 9:38:01,adrienne fong,adriennekfong@gmail.com,5109991262,3,Web Development,Mobile App Development,Artificial Intelligence
+1/31/2022 17:54:35,Matteo Terrien,matteo.terrien@gmail.com,9192603551,4,Artificial Intelligence,Mobile App Development,(General) App Development
+2/1/2022 0:15:12,Matthew Prado,matthew.f.prado@valkyriepcs.com,9252697163,4,(General) App Development,Data Science,Web Development
+2/6/2022 0:12:28,Anthony,anthonybarnard001@gmail.com,7076557700,4,Web Development,Game Development,(General) App Development
+1/22/2022 15:51:03,Brian Chung,brrc88@gmail.com,9255292978,5,Game Development,(General) App Development,Artificial Intelligence
+1/22/2022 15:54:13,Julie,jackjack170827@gmail.com,6464210047,5,(General) App Development,Data Science,Artificial Intelligence
+1/22/2022 20:07:54,Ashley Alvarez,Ash19641994@gmail.com,9254997128,5,(General) App Development,Web Development,Artificial Intelligence
+1/26/2022 10:08:25,Ibrahim Shoukry,Ibrahim.m.shoukry@gmail.com,3413140266,5,Data Science,Web Development,(General) App Development
+1/29/2022 10:43:44,Charles Qu,charlesqu22@gmail.com,5307606002,5,Data Science,(General) App Development,Artificial Intelligence
+1/31/2022 17:54:11,Ashan Devine,ashandevine@yahoo.com,9258993094,5,Artificial Intelligence,Data Science,Web Development
+1/31/2022 17:58:21,Subrat Tripathi,subrat.tripathi@gmail.com,5105147249,5,(General) App Development,Web Development,Data Science
+1/29/2022 19:31:02,Harshini Thavathiru murugan,harshini.t1234@gmail.com,9253680871,6,Data Science,Artificial Intelligence,(General) App Development
+1/31/2022 12:34:13,Yuheon Joh (John),johyuheon@gmail.com,9257449811,6,Artificial Intelligence,Data Science,Web Development
+1/31/2022 17:53:38,John Chen,johneeeeeeeeee3@gmail.com,9253201788,6,Data Science,Artificial Intelligence,(General) App Development
+1/31/2022 17:56:30,Nicolas Sukardy,whatthegamers0@gmail.com,5109935318,6,Game Development,Web Development,Artificial Intelligence
+1/31/2022 18:07:14,Maddie Ananda,maddieananda@gmail.com,92353258796,6,Game Development,Artificial Intelligence,Mobile App Development
+2/4/2022 13:51:15,Safia Boutaleb,safiaboutalebab@gmail.com,9255755998,6,Mobile App Development,Game Development,(General) App Development
+2/4/2022 23:05:30,Matthew Lim,personal.matthewjlim@gmail.com,9257196872,6,Data Science,Artificial Intelligence,(General) App Development
+2/4/2022 23:31:53,Gabe Uy,randolfuy09@gmail.com,9258090378,6,Artificial Intelligence,Mobile App Development,Mobile App Development
+2/7/2022 10:44:45,Shruti Sengottuvel,shrutisen03@gmail.com,9259980764,6,Web Development,(General) App Development,Artificial Intelligence
+1/23/2022 22:03:41,Wilson Zhu,w.z1654@gmail.com,5108163608,7,(General) App Development,Artificial Intelligence,Mobile App Development
+1/24/2022 18:16:53,Jonathan Saleh,jonathan160720032@gmail.com,9254898004,7,Web Development,Mobile App Development,Artificial Intelligence
+1/31/2022 14:19:10,Camden Beard,cbeard644@insite.4cd.edu,8123205706,7,(General) App Development,Game Development,Artificial Intelligence
+1/31/2022 22:00:23,Brian Leong,gameboy0429@gmail.com,9257868694,7,(General) App Development,Game Development,Data Science
+2/4/2022 13:53:39,Divya Krishnan,divya.krishnan445@gmail.com,9252024926,7,(General) App Development,Artificial Intelligence,Data Science
+1/29/2022 13:41:19,Konstantin Victoria,konstantinvictoria@gmail.com,4158890008,9,Artificial Intelligence,Data Science,Web Development`;
+//data prep
+let data_entries = {};
+let preference_groups = {};
+
+data.split("\n").forEach((line, i) => {
+  if (!i) return;
+  let entry = line.split(",");
+  let user_properties = {
+    name: entry[1],
+    contact: entry[2],
+    number: entry[3],
+    aptitude: entry[4],
+    preferences: entry.slice(5),
+  };
+
+  split_by_name(entry, user_properties);
+  split_by_preferences(entry, user_properties);
+});
+
+export let preferences = preference_groups;
+export function start_sort(threshold, preference) {
+  let set_of_people = [];
+  let group = [];
+  let num_of_people = 0;
+  let total_aptitude = 0;
+  let unsorted_groups = [];
+  let execute_start_time = new Date().getTime();
+  let preference_name = Object.keys(preference_groups)[preference];
+  Object.values(Object.entries(preference_groups)[preference][1]).forEach(
+    (person) => {
+      if (group.length == 4) {
+        unsorted_groups.push(group);
+        group = [];
+      }
+      group.push(person);
+      set_of_people.push(person);
+      num_of_people++;
+      total_aptitude += parseInt(person.aptitude);
+    }
+  );
+  let standard_deviation = 0;
+  const average_aptitude = total_aptitude / num_of_people;
+  //data prep end
+
+  //sort begin
+  let sort_report = "";
+  unsorted_groups = groupings_objectify(unsorted_groups, average_aptitude);
+  let sorted_groups_deviation = -1;
+  //let threshold = 1;
+
+  //  new Date().getTime() - execute_start_time <= 4000
+  let sorted_groups = {};
+  for (
+    let k = 0;
+    sorted_groups_deviation === -1 || sorted_groups_deviation > threshold;
+    k++
+  ) {
+    let sorted_people = set_of_people;
+    if (k != 0) {
+      sorted_people = sort(set_of_people);
+    }
+    for (let i = 0; i < k; i++) {
+      sorted_people = sort(sorted_people);
+      if (new Date().getTime() - execute_start_time > 4000) {
+        return {
+          is_successful: 0,
+          payload: {
+            data: "Timed Out. Try matching the last standard deviation",
+            standard_deviation: sorted_groups_deviation,
+          },
+        };
+      }
+    }
+
+    let groupings_sorted = split_into_groups_of(4, sorted_people);
+    sorted_groups = groupings_objectify(groupings_sorted, average_aptitude);
+    sorted_groups.aptitude_vector.forEach(
+      (element, i) =>
+        (sort_report +=
+          `"${element}"` +
+          (i !== sorted_groups.aptitude_vector.length - 1 ? "," : ""))
+    );
+    sort_report += "\n";
+    let sorted_groups_average_aptitude =
+      sorted_groups.aptitude_vector.reduce((partial_sum, a) => {
+        return partial_sum + a;
+      }, 0) / sorted_groups.aptitude_vector.length;
+
+    sorted_groups_deviation = Math.sqrt(
+      sorted_groups.aptitude_vector.reduce((partial_sum, a) => {
+        return partial_sum + Math.pow(a - sorted_groups_average_aptitude, 2);
+      }, 0) / sorted_groups.aptitude_vector.length
+    );
+    if (k == 0) {
+      shuffle_array(sorted_people);
+    }
+  }
+  let group_listing = {};
+  group_listing[preference_name] = {};
+  sorted_groups.vector.forEach((group, i) => {
+    group_listing[preference_name][`Group ${i + 1}`] = [];
+    group_listing[preference_name][`Group ${i + 1}`] = group.map((person) => {
+      return [person.name, person.aptitude];
+    });
+  });
+  return {
+    is_successful: 1,
+    payload: {
+      data: group_listing,
+      standard_deviation: sorted_groups_deviation,
+    },
+  };
+}
+//sort end
+
+//functions
+function split_by_name(entry, user_properties) {
+  data_entries[entry[1]] = user_properties;
+}
+
+function split_by_preferences(entry, user_properties) {
+  for (let i = 5; i <= 7; i++) {
+    if (!preference_groups[entry[i]]) {
+      preference_groups[entry[i]] = {};
+    }
+    preference_groups[entry[i]][entry[1] ? entry[1] : entry[2]] =
+      user_properties;
+  }
+}
+
+function shuffle_array(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+function split_into_groups_of(number_of_people, array) {
+  let groupings_sorted = [];
+  let group = [];
+  for (let i = 0; i < array.length; i++) {
+    group.push(array[i]);
+    if (group.length == number_of_people) {
+      groupings_sorted.push(group);
+      group = [];
+    }
+  }
+  if (group.length != 4) {
+    groupings_sorted.push(group);
+  }
+
+  return groupings_sorted;
+}
+
+function deep_sort(array) {
+  let half_index = Math.ceil(array.length / 2);
+  let left_half_of_array = array.slice(0, half_index);
+  let right_half_of_array = array.slice(-half_index);
+  if (left_half_of_array.length > 2) {
+    left_half_of_array = deep_sort(left_half_of_array);
+  }
+  if (right_half_of_array.length > 2) {
+    right_half_of_array = deep_sort(right_half_of_array);
+  }
+  let left_half_total_aptitude = left_half_of_array.reduce(
+    (partialSum, { aptitude }) => partialSum + parseInt(aptitude),
+    0
+  );
+  let right_half_total_aptitude = right_half_of_array.reduce(
+    (partialSum, { aptitude }) => partialSum + parseInt(aptitude),
+    0
+  );
+  for (let i = 0; i < right_half_of_array.length - i - 1; i++) {
+    if (
+      exchange_rule(
+        left_half_of_array[i],
+        right_half_of_array[right_half_of_array.length - i - 1]
+      )
+    ) {
+      let middle_man = left_half_of_array[i];
+      left_half_of_array[i] =
+        right_half_of_array[right_half_of_array.length - i - 1];
+      right_half_of_array[right_half_of_array.length - i - 1] = middle_man;
+    }
+  }
+
+  function exchange_rule(agent_left, agent_right) {
+    const leeway = 20;
+    let net_agent_aptitude = agent_right.aptitude - agent_left.aptitude;
+    let net_group_aptitude =
+      right_half_total_aptitude - left_half_total_aptitude;
+    let trade_net_group_aptitude =
+      left_half_total_aptitude +
+      net_agent_aptitude -
+      (left_half_total_aptitude - net_agent_aptitude);
+
+    if (Math.abs(net_group_aptitude) > Math.abs(trade_net_group_aptitude))
+      return true;
+    return false;
+  }
+  return left_half_of_array.concat(right_half_of_array);
+  //console.log(left_half_of_array.length, right_half_of_array.length);
+}
+
+function sort(array) {
+  return [...new Set(deep_sort(array))];
+}
+
+function groupings_objectify(vector, average_aptitude) {
+  function group_aptitude_vector(groupings) {
+    return groupings.map((group) => {
+      let group_aptitude = 0;
+      group.forEach((person) => {
+        group_aptitude += parseInt(person.aptitude);
+      });
+      return group_aptitude;
+    });
+  }
+  function group_aptitude_greediness_vector(group_vector, average_aptitude) {
+    return group_vector.map((total_aptitude) => {
+      return total_aptitude - average_aptitude;
+    });
+  }
+  let aptitude_vector = group_aptitude_vector(vector);
+  let aptitude_greediness_vector = group_aptitude_greediness_vector(
+    aptitude_vector,
+    average_aptitude
+  );
+  let vector_object = {
+    vector: vector,
+    aptitude_vector: aptitude_vector,
+    aptitude_greediness_vector: aptitude_greediness_vector,
+  };
+  return vector_object;
+}
+
+// console.log(average_aptitude);
+// console.log(group_aptitude_greediness_vector);
